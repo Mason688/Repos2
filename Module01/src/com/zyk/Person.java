947df74a260be4a3b10e2d594945d1cda375a6a5
Mason <365@x1.tn>
2022-04-23 21:20:33 +0800

    Person

---
+++ Module01/src/com/zyk/Person.java
@@ -0,0 +1,16 @@
+/**
+ * fileName： Person.java
+ * author：   zyk
+ * date：     2022-4-23
+ */
+package com.zyk;
+
+/**
+ * @className Person
+ * @author zyk
+ * @caeate 2022-4-23 21:18
+ */
+public class Person {
+    private int age;
+    
+}
